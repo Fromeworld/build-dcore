ef1ae27c9591b605d272bac435349c4347e2848a
haixin <qiuhaixin1101@gmail.com>
2020-03-11 22:00:59 +0100

    bash ->sh

--- build-dcore.sh
+++ build-dcore.sh
@@ -8,26 +8,26 @@ mkdir -p deps && cd deps
 
 echo "installing from ppa and pip"
 
-bash ./install_deps.sh
+sh ./install_deps.sh
 
 echo "installing triqs"
 
-bash ./install_triqs.sh
+sh ./install_triqs.sh
 
 echo "installing DFTTools"
 
-bash ./install_DFTTools.sh
+sh ./install_DFTTools.sh
 
 echo "installing ALPSCore"
 
-bash ./install_ALPSCore.sh
+sh ./install_ALPSCore.sh
 
 echo "installing CT-HYB"
 
-bash ./install_CT-HYB.sh
+sh ./install_CT-HYB.sh
 
 echo "DCore"
 
-bash ./install_DCore.sh
+sh ./install_DCore.sh
 
 echo "done"
