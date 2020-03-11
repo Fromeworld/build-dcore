e7eb55cf331cfc00cf91b11fde5d88e967a76ff2
Fromeworld <qiuhaixin1101@gmail.com>
2020-03-11 23:52:36 +0100

    fix mistake

--- install_ALPSCore.sh
+++ install_ALPSCore.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # 下载源代码并解压
-curl -Lo triqs-2.1.1.tar.gz https://github.com/TRIQS/triqs/archive/2.1.1.tar.gz
-tar zxvf triqs-2.1.1.tar.gz
+curl -Lo alpscore-v2.2.0.tar.gz https://github.com/ALPSCore/ALPSCore/archive/v2.2.0.tar.gz
+tar zxvf alpscore-v2.2.0.tar.gz
 
-# 设置安装目录
-INSTALL_PREFIX=$(pwd)/triqs
+# 设置 ALPSCore 安装位置
+INSTALL_PREFIX=$(pwd)/ALPSCore
 
 # 设置编译所用的核心数
 NCORES=6
 
 # 用 cmake 配置编译步骤
-mkdir -p triqs.build && cd triqs.build
-cmake ../triqs-2.1.1 -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX
+mkdir -p ALPSCore.build && cd ALPSCore.build
+cmake ../ALPSCore-2.2.0 -DALPS_INSTALL_EIGEN=yes -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX
 
 # 编译，测试和安装
 make -j$NCORES && make test && make install
--- install_triqs.sh
+++ install_triqs.sh
@@ -10,7 +10,7 @@ INSTALL_PREFIX=$(pwd)/triqs
 # 设置编译所用的核心数
 NCORES=6
 
-# 用 cmake 配置编译步骤 disabled tests
+# 用 cmake  disabled tests otherwise take forever (problem when 60% test/...bubble, 100% disk usage)
 mkdir -p triqs.build && cd triqs.build
 cmake ../triqs-2.1.1 -DCMAKE_INSTALL_PREFIX=$INSTALL_PREFIX -DBuild_Tests=OFF
 
