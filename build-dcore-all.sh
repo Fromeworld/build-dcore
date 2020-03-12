22a4c7a8a8141f62bb9bc000059d611318765752
Fromeworld <qiuhaixin1101@gmail.com>
2020-03-12 01:01:41 +0100

    comment

--- build-dcore-all.sh
+++ build-dcore-all.sh
@@ -5,13 +5,13 @@
 mkdir -p deps && cd deps
 
 # ======================
-# 2. install deps from ppa
+# 2. install deps by ubuntu apt and then pip
 sudo apt-get install cmake g++ gfortran git hdf5-tools \
      libblas-dev libboost-all-dev libfftw3-dev libgfortran3 \
      libhdf5-serial-dev libgmp-dev liblapack-dev libopenmpi-dev \
      python-dev python-h5py python-jinja2 python-mako python-matplotlib \
      python-mpi4py python-numpy python-scipy python-tornado python-zmq \
-&& pip install enum34
+&& pip install enum34 # for dcore test
 
 # ======================
 # 3. install triqs
