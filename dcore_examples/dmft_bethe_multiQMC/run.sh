954f1f6516b0ad3307c68beae7bf60b232778436
Fromeworld <qiuhaixin1101@gmail.com>
2020-03-16 23:17:30 +0100

    add folder for examplex

---
+++ dcore_examples/dmft_bethe_multiQMC/run.sh
@@ -0,0 +1,3 @@
+dcore_pre dmft_bethe.ini
+dcore dmft_bethe.ini
+dcore_post dmft_bethe.ini
---
+++ dcore_examples/env.sh
@@ -0,0 +1,8 @@
+# set all env
+
+export PATH=$PATH:~/opt/dcore/bin
+export PATH=$PATH:~/opt/alps_cthyb/bin
+export PATH=$PATH:~/opt/dcore/bin
+export PATH=$PATH:~/opt/triqs/bin
+export MPIRUN="mpirun"
+
